b454dbf79c6d22f556851e659e654c894b1b2b0d
Patricio Dibernardi <dibernardi.patricio@gmail.com>
2021-05-31 23:49:06 -0300

    server.js

--- publish/app.js
+++ publish/app.js
@@ -3,26 +3,29 @@ var port = process.env.PORT || 3000,
     fs = require('fs'),
     html = fs.readFileSync('index.html');
 
-var log = function(entry) {
+var log = function (entry) {
     fs.appendFileSync('/tmp/sample-app.log', new Date().toISOString() + ' - ' + entry + '\n');
 };
+
+
+
 
 var server = http.createServer(function (req, res) {
     if (req.method === 'POST') {
         var body = '';
 
-        req.on('data', function(chunk) {
+        req.on('data', function (chunk) {
             body += chunk;
         });
 
-        req.on('end', function() {
+        req.on('end', function () {
             if (req.url === '/') {
                 log('Received message: ' + body);
             } else if (req.url = '/scheduled') {
                 log('Received task ' + req.headers['x-aws-sqsd-taskname'] + ' scheduled at ' + req.headers['x-aws-sqsd-scheduled-at']);
             }
 
-            res.writeHead(200, 'OK', {'Content-Type': 'text/plain'});
+            res.writeHead(200, 'OK', { 'Content-Type': 'text/plain' });
             res.end();
         });
     } else {
