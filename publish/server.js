266a3e6b872dbf493f1167d1b79ea11f1c7b023b
Patricio Dibernardi <dibernardi.patricio@gmail.com>
2021-05-31 23:40:01 -0300

    nodecommand

--- publish/server.js
+++
@@ -1,8 +0,0 @@
-var express = require('express');
-var app = express();
-var port = process.env.PORT || process.argv[2] || 80;
-app.use('/', express.static(__dirname + '/www'));
-var server = app.listen(port, function () {
-    console.log('listening on port:', port);
-});
-//# sourceMappingURL=server.js.map
